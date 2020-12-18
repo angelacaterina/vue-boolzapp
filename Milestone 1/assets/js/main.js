22999e97998582e4a3c89baa30d725898b26c29e
angelacaterina <angela.caterina@icloud.com>
2020-12-18 19:21:37 +0100

    img con vue

---
+++ Milestone 1/assets/js/main.js
@@ -0,0 +1,51 @@
+
+//  Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+let app = new Vue({
+  el:'#app',
+  data:{
+    counter: 0,
+    message: 'Ultimo accesso oggi alle',
+    contacts: [
+      {
+        name: 'Michele',
+        avatar: '_1',
+        visible: true
+      },
+      {
+        name: 'Fabio',
+        avatar: '_2',
+        visible: true
+      },
+      {
+        name: 'Samuele',
+        avatar: '_3',
+        visible: true
+      },
+      {
+        name: 'Franco',
+        avatar: '_4',
+        visible: true
+      },
+      {
+        name: 'Antonio',
+        avatar: '_5',
+        visible: true
+      },
+      {
+        name: 'Luisa',
+        avatar: '_6',
+        visible: true
+      },
+      {
+        name: 'Marco',
+        avatar: '_7',
+        visible: true
+      },
+      {
+        name: 'Andrea',
+        avatar: '_8',
+        visible: true
+      }
+    ]
+  }
+})
